15efff99889b556a124b84c915815b78a5a8cdc7
Abagail Hudson <abagailanne@gmail.com>
2019-09-02 20:33:15 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,12 @@ class PostsController < ApplicationController
 	  redirect_to post_path(@post)
 	end
 
+	private
+ 
+	def post_params
+  	params.require(:post).permit(:title, :description)
+	end
+
 	def edit
 	  @post = Post.find(params[:id])
 	end
